63aa2910fd61dee2a5b55c68d60b07572f166853
ajcairel <alexanderjcairel@gmail.com>
2022-08-02 21:05:22 -0700

    Picture and Insta

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,10 +29,10 @@ export default function NavBar() {
                     Skills
                   </a>
                   <a
-                    href="#projects"
+                    href="#contact"
                     className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
                   >
-                    My Work
+                    Contact Me
                   </a>
                 </div>
               </div>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,36 +2,40 @@ export default function Welcome() {
     return (
       <section id="about">
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center pb-0">
+        <div className="lg:max-w-lg lg:w-1/2 md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="./Jake.png"
+            />
+          </div>
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-              Alexander Cairel
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
+              Jake Bohland
               <br className="hidden lg:inline-block" />
             </h1>
             <h3 className="sm:text-4xl text-xl mb-4 font-medium text-slate-400">
-              Software Engineer
+              Brown Belt under Marcio Morera
             </h3>
-            <p className="mb-8 leading-relaxed text-white">
-              Experienced Project Manager and software engineering bootcamp grad. My
-              experience working in cross-functional teams and developing
-              applications has taught me to optimize available resources in
-              delivering a quality product.
+            <p className="mb-8 leading-relaxed text-black">
+              I like to eat apples.
             </p>
             <div className="lg:flex sm:flex-wrap">
               <div className="p-2 w-full">
                 <a
-                  href="https://www.linkedin.com/in/alexandercairel/"
+                  href="https://www.instagram.com/jakebohland_mmbjj/"
                   target="_blank"
                   rel="noopener"
                 >
                   <div className="bg-gray-800 rounded flex p-4 h-full items-center">
                     <img
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
-                      alt="The logo icon for LinkedIn"
-                      title="LinkedIn"
+                      src="./instagram.png"
+                      alt="The logo icon for insta(OG)"
+                      title="Insta"
                       className="w-6 h-6 flex-shrink-0 mr-4"
                     />
                     <span className="title-font font-medium text-white">
-                      LinkedIn
+                      Instagram
                     </span>
                   </div>
                 </a>
@@ -91,13 +95,13 @@ export default function Welcome() {
               </div>
             </div>
           </div>
-          <div className="lg:max-w-lg lg:w-1/2 md:w-1/2 w-5/6">
+          {/* <div className="lg:max-w-lg lg:w-1/2 md:w-1/2 w-5/6">
             <img
               className="object-cover object-center rounded"
               alt="hero"
-              src="./pfp.png"
+              src="./Jake.png"
             />
-          </div>
+          </div> */}
         </div>
       </section>
     );
