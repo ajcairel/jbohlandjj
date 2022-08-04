fa1ffd3250ded40ab429622d3352f934609e42be
ajcairel <alexanderjcairel@gmail.com>
2022-08-03 19:22:45 -0700

    Green Submit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -89,7 +89,7 @@ export default function Contact() {
           </div>
           <button
             type="submit"
-            className="text-white bg-blue-300 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            className="text-white bg-blue-300 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
             Submit
           </button>
         </form>
